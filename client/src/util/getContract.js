e19ebd5773f7b561d36f929e1c1e32d5d3a8a460
Jacky <13690913046@163.com>
2019-07-30 11:37:45 +0800

    修复引用bug

--- client/src/util/getContract.js
+++ client/src/util/getContract.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3'
-import { address, ABI } from './comtractsAbi/Account'
+// import { address, ABI } from './comtractsAbi/Account'
 import AccountContract from '../../build/contracts/Account'
 
 let getContract = new Promise((resolve, rejects) => {
